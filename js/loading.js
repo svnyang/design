f1c4346f3e50b3293acae40e61c0cae7b5c060ec
Boss-eng <85914318+Boss-eng@users.noreply.github.com>
2021-07-27 13:40:42 +0800

    v3

--- js/loading.js
+++ js/loading.js
@@ -6,7 +6,8 @@ var _LoadingTop = _PageHeight > 230 ? (_PageHeight - 230) / 2 : 0,
 _LoadingLeft = _PageWidth > 230 ? (_PageWidth - 230) / 2 : 0;
 //在页面未加载完毕之前显示的loading Html自定义内容  
 $('body').addClass('modal-active');
-  var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight +  'px;top:0;background:#FFFFFF;opacity:1.0;filter:alpha(opacity=80);z-index:10000;"><div class="spinner" style="position: top: 60px; margin:' + _LoadingTop + 'px auto ;   width: 230px;height: 230px;background-color: #67CF22;background: url(images/tsp-pro.gif) no-repeat;background-size: 100% 100%;"></div></div>';
+  // var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight +  'px;top:0;background:#FFFFFF;opacity:1.0;filter:alpha(opacity=80);z-index:10000;"><div class="spinner" style="position: top: 60px; margin:' + _LoadingTop + 'px auto ;   width: 230px;height: 230px;background-color: #67CF22;background: url(images/tsp-pro.gif) no-repeat;background-size: 100% 100%;"></div></div>';
+  var _LoadingHtml = '<div id="loadingDiv" style="position: fixed;top: 0;left: 0;width: 100%;height: 100%;background:#FFFFFF;opacity:1.0;filter:alpha(opacity=80);z-index:10000;"><div class="spinner" style="position: top: 60px; margin:' + _LoadingTop + 'px auto ;   width: 230px;height: 230px;background-color: #67CF22;background: url(images/tsp-pro.gif) no-repeat;background-size: 100% 100%;"></div></div>';
 //呈现loading效果  
 document.write(_LoadingHtml);
 //监听加载状态改变  
